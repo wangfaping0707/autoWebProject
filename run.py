64f64255222a9248896f457654e92099777df6fd
wangfaping0707 <1032683901@qq.com>
2024-03-20 17:44:58 +0800

    '优化代码执行方式'

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
+import os
+import time
+
 import pytest
 
 if __name__ == '__main__':
-    pytest.main()
+	# pytest.main(['testcase/t_pytest/test_params_one.py'])
+	# pytest.main(['testcase/t_fixture/test_fix3_params.py'])
+	# pytest.main(['testcase/t_allure/test_allure_demo.py'])
+	pytest.main(['testcases/test_search_baidu.py'])
+	time.sleep(3)
+	os.system("allure generate outputs/allure_results_files -o outputs/html/reports.html --clean")
+	# time.sleep(5)
+	# # 自动打开测试报告
+	# os.system("allure open ./outputs/html/reports.html")
